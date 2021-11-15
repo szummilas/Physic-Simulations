96d33d0db9e35e64339c6b2d3fb77b3faef81a28
SonOfGrabarz <m.szul00@gmail.com>
2021-11-15 18:38:17 +0100

    euler3

--- dyski-eulera/main.py
+++ dyski-eulera/main.py
@@ -1,38 +1,40 @@
-import pygame
-import random
+import math
 
-pygame.init()
-WIDTH = 1280
-HEIGHT = 720
-resolution = [WIDTH, HEIGHT]
-BACKGROUND = pygame.image.load("background.jpg")
-WHITE = (255, 255, 255)
-PINK = (255, 63, 180)
-LIME = (208, 255, 63)
-BLUE = (99, 63, 255)
-PURPLE = (103, 160, 237)
+from config import *
 
-screen = pygame.display.set_mode(resolution)
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('Euler\'s disks')
-
+pygame.init()
 pygame.display.flip()
-printing = True
-TARGET_FPS = 30
 clock = pygame.time.Clock()
-gravity = 0.9
-CENTER = pygame.math.Vector2(WIDTH / 2, HEIGHT / 2)
 
 Dict = {
     1: 'gravity',
     2: 'attract',
-    3: 'aerodynamics'
+    3: 'aerodynamics',
+    4: 'point'
 }
+
+slider1 = thorpy.SliderX(100, (1, 1000), "ELEMENTS", type_=int, initial_value=100)
+slider1.set_font_color(WHITE)
+slider2 = thorpy.SliderX(120, (1, 10), "SIZE", type_=int)
+slider2.set_font_color(WHITE)
+box = thorpy.Box(elements=[slider1, slider2])
+menu = thorpy.Menu(box)
+
+for element in menu.get_population():
+    element.surface = screen
+
+box.set_topleft((0, 0))
+box.set_main_color((255, 255, 255, 100))
 
 
 # Euler's disks
 class Circle:
     def __init__(self):
+        self.mass = 2
         self.g = 0.9
+        self.gravity = 9.81
 
         # random position
         self.position = pygame.math.Vector2(random.randint(0, WIDTH), random.randint(0, HEIGHT))
@@ -46,19 +48,24 @@ class Circle:
         # force vector
         self.force = pygame.math.Vector2(0, 0)
 
-    def update(self, mode, delta_time):
+        # randomize
+        self.random_color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+
+    def update(self, mode, delta_time, size):
         if mode == 'gravity':
             # y = y + v * dt
             self.position = self.position + self.velocity * delta_time
 
             # v(y) = v(y) + g * dt
             self.velocity.y = self.velocity.y + self.g * delta_time
+
+            # reset velocity in x axis
             self.velocity.x = 0.0
 
             # screen borders
             if self.position.y + self.velocity.y < 0 or self.position.y + self.velocity.y > HEIGHT:
                 self.velocity.y = -self.velocity.y
-            if self.position.x + self.velocity.x < 0 or self.position.x + self.velocity.x > HEIGHT:
+            if self.position.x + self.velocity.x < 0 or self.position.x + self.velocity.x > WIDTH:
                 self.velocity.x = -self.velocity.x
 
         elif mode == 'attract':
@@ -90,12 +97,24 @@ class Circle:
                 self.position.y = 0
                 self.velocity.y *= -1.0
 
-    def draw(self, display, color):
-        pygame.draw.circle(display, color, self.position, 4)
+        elif mode == 'aerodynamics':
+            self.velocity.y += self.gravity * self.mass
+            self.velocity += -6 * math.pi * 0.00001708 * size * self.velocity
+            self.position += self.velocity
+
+            # screen borders
+            if self.position.y + self.velocity.y < 0 or self.position.y + self.velocity.y > HEIGHT:
+                self.velocity.y = -self.velocity.y
+            if self.position.x + self.velocity.x < 0 or self.position.x + self.velocity.x > WIDTH:
+                self.velocity.x = -self.velocity.x
+
+    def draw(self, display, color, size):
+        pygame.draw.circle(display, color, self.position, size)
 
 
 circles_list = [Circle() for _ in range(0, 1000)]
 running = True
+coloring = False
 current_mode = Dict[1]
 COLOR = PINK
 while running:
@@ -109,18 +128,40 @@ while running:
                 COLOR = PINK
                 TARGET_FPS = 30
                 current_mode = Dict[1]
+
             if event.key == pygame.K_2:
                 COLOR = LIME
                 TARGET_FPS = 60
                 current_mode = Dict[2]
+
+            if event.key == pygame.K_3:
+                COLOR = BLUE
+                TARGET_FPS = 30
+                current_mode = Dict[3]
+
             if event.key == pygame.K_SPACE:
                 circles_list.clear()
                 circles_list = [Circle() for _ in range(0, 1000)]
 
+            if event.key == pygame.K_r:
+                if not coloring:
+                    coloring = True
+                else:
+                    coloring = False
+
+        menu.react(event)
+
     screen.blit(BACKGROUND, (0, 0))
 
-    for i in range(len(circles_list)):
-        circles_list[i].draw(screen, COLOR)
-        circles_list[i].update(current_mode, dt)
+    for i in range(0, slider1.get_value()):
+        if coloring:
+            circles_list[i].draw(screen, circles_list[i].random_color, slider2.get_value())
+        else:
+            circles_list[i].draw(screen, COLOR, slider2.get_value())
+
+        circles_list[i].update(current_mode, dt, slider2.get_value())
+
+    box.blit()
+    box.update()
 
     pygame.display.update()
